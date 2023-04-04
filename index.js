22607fea63f126d49edc95d3d7bf37f388e5dd45
Emman77240 <asiegbuemmanuelekee@gmail.com>
2023-04-04 19:32:28 +0100

    Ready to deploy backend server

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
+const cors = require("cors");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB Connection Successful!"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+// Whitelist Client URL
+const whitelist = ["https://sweet-dragon-9c8740.netlify.app/", "http://localhost:3000"]
+const corsOptions = {
+  origin: function(origin, callback) {
+    if(!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error("Not allowed by CORS"))
+    }
+  }, 
+  credenials: true
+}
+
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Backend server is running!");
+});
